0851ad5c2b723a3a2191c27e3a66aa4b14eaf033
Dmytro Karpus <dimas.karpusss@gmail.com>
2023-04-23 22:15:40 +0200

    #2 task 4

--- practice/src/tasks/tasks.py
+++ practice/src/tasks/tasks.py
@@ -57,5 +57,87 @@ def task_2():
 
     plt.show()
 
+
+def task_3():
+    from sklearn.linear_model import LinearRegression
+    N = 1000
+    n = 1000
+
+    # Generate x values from a uniform distribution between -1 and 1
+    x = np.random.uniform(-1, 1, n)
+
+    # Generate the error term from a standard normal distribution
+    e = np.random.normal(0, 1, n)
+
+    # Generate y values
+    y = x**2 + e
+
+    # Split the dataset into two parts based on the sign of x
+    x_neg = x[x < 0]
+    y_neg = y[x < 0]
+    x_pos = x[x >= 0]
+    y_pos = y[x >= 0]
+
+    # Train a linear regression model on the first part of the dataset (x < 0)
+    lr = LinearRegression()
+    lr.fit(x_neg.reshape(-1, 1), y_neg)
+
+    # Print the coefficients and R^2 score
+    print("Intercept:", lr.intercept_)
+    print("Coefficient:", lr.coef_)
+    print("R^2 score:", lr.score(x_neg.reshape(-1, 1), y_neg))
+
+    # Calculate the residuals on the test set (x > 0)
+    residuals = y_pos - lr.predict(x_pos.reshape(-1, 1))
+
+    # Plot the histogram of residuals
+    plt.hist(residuals, bins=20)
+    plt.title("Histogram of Residuals")
+    plt.xlabel("Residuals")
+    plt.ylabel("Frequency")
+    plt.show()
+
+    # Calculate the mean squared error (MSE) on the training set (x < 0)
+    mse_train = np.mean((y_neg - lr.predict(x_neg.reshape(-1, 1)))**2)
+    print("MSE on training set:", mse_train)
+
+    # Calculate the mean squared error (MSE) on the test set (x > 0)
+    mse_test = np.mean((y_pos - lr.predict(x_pos.reshape(-1, 1)))**2)
+    print("MSE on test set:", mse_test)
+
+
+def task_4():
+    N = 5000
+    
+    x = np.random.uniform(-1, 1, N);
+    e = 0.2 * np.random.normal(0,1, N)
+    y = x**2 + e
+
+    x_neg = x[x < 0].reshape(-1,1)
+    x_pos = x[x >= 0].reshape(-1,1)
+    y_neg = y[x < 0]
+    y_pos = y[x >= 0]
+
+    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.linear_model import LinearRegression
+    
+    pf = PolynomialFeatures(2)
+    x2_pos = pf.fit_transform(x_pos)
+    x2_neg = pf.fit_transform(x_neg)
+
+    lr = LinearRegression()
+    lr.fit(x2_pos, y_pos)
+
+    print('Intercept:', lr.intercept_)
+    print('Coefficient:', lr.coef_)
+    print('R^2 score: ', lr.score(x2_neg, y_neg))
+
+    residuals = y_pos - lr.predict(x2_pos)
+    
+    plt.hist(residuals, bins=20)
+    plt.show()
+
+    print('MSE', np.mean((y_neg - lr.predict(x2_neg))**2))
+
 def main():
-    task_2()
+    task_4()
