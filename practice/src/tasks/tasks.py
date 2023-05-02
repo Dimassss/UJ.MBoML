e823e93e06ecee15633e70b9bbccc1a8fa224ac2
Dmytro Karpus <dimas.karpusss@gmail.com>
2023-05-02 02:40:42 +0200

    #lec/6 task 2

--- practice/src/tasks/tasks.py
+++ practice/src/tasks/tasks.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.model_selection import GridSearchCV
+import sklearn.preprocessing as pr
+import sklearn.model_selection as ms
 
 matplotlib.use('TkAgg')
 
@@ -46,5 +48,30 @@ def task_1():
     plt.legend()
     plt.show()
 
+def task_2():
+    x=np.linspace(0,2*np.pi,40)
+    y=np.sin(x)+np.random.normal(0,0.4,40)
+
+    pf = pr.PolynomialFeatures(17)
+
+    x_train, x_test, y_train, y_test = ms.train_test_split(pf.fit_transform(x.reshape(-1,1)),y)
+
+
+    lasso = Lasso()
+    gs = GridSearchCV(lasso, {'alpha': np.logspace(-2,2, 50)}, cv=6, scoring='neg_mean_absolute_error')
+    gs.fit(x_train, y_train)
+    
+    lasso = Lasso(alpha=gs.best_params_['alpha'])
+    lasso.fit(x_train, y_train)
+
+    print("Score:", lasso.score(x_test, y_test))
+    print("Best alpha: ", gs.best_params_['alpha'])
+
+    ox = np.linspace(min(x), max(x), 100)
+    plt.plot(ox, lasso.predict(pf.fit_transform(ox.reshape(-1,1))))
+
+    plt.scatter(x,y)
+    plt.show()
+
 def main():
-    task_1()
+    task_2()
