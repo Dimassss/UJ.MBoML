8b8395d3f42dd57956845e4b4529968d5129a74a
Dmytro Karpus <dimas.karpusss@gmail.com>
2023-05-25 01:54:39 +0200

    #10 task 2 is done

--- practice/src/tasks/tasks.py
+++ practice/src/tasks/tasks.py
@@ -3,15 +3,14 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+
 
 def task_1():
-
-    # sigmoid function
-    def nonlin(x,deriv=False):
-        if(deriv==True):
-            return x*(1-x)
-        return 1/(1+np.exp(-x))
-
     X = np.array([  
         [1,1,1],
         [1,0,1],
@@ -48,5 +47,49 @@ def task_1():
     print(np.mean([err(k) for k in range(len(y))]))
 
 
+def task_2(n):
+    # n = number of neurons in hidden layer
+    X = np.array([  
+        [1,1,1],
+        [1,0,1],
+        [0,1,1],
+        [0,0,1],
+        [0,0,0],
+        [1,1,0]
+    ])
+
+    y = np.array([  
+        [1,1],
+        [1,1],
+        [1,0],
+        [0,0],
+        [0,0],
+        [1,1]
+    ])
+
+    W1 = 2*np.random.random((3,n)) - 1
+    W2 = 2*np.random.random((n,2)) - 1
+
+    for i in range(1000):
+        l0 = X
+        l1 = nonlin(np.dot(l0, W1))
+        l2 = nonlin(np.dot(l1, W2))
+
+        l2_error = y - l2
+        l2_delta = l2_error * nonlin(l2, True)
+
+        l1_error = l2_delta.dot(W2.T)
+        l1_delta = l1_error * nonlin(l1, True)
+
+        W1 += l0.T.dot(l1_delta)
+        W2 += l1.T.dot(l2_delta)
+
+
+    l0 = X
+    l1 = nonlin(np.dot(l0, W1))
+    l2 = nonlin(np.dot(l1, W2))
+
+    print(np.mean(np.abs(l2 - y)))
+
 def main():
-    task_1()
+    task_2(20)
